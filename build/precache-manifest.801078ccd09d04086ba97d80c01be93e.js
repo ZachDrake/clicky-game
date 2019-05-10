af194f27af06c54e9cfa182670d63033efed3146
Zach Drake <zadrake@yahoo.com>
2019-05-10 12:57:51 -0700

    deploy

---
+++ build/precache-manifest.801078ccd09d04086ba97d80c01be93e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdcefc989456f582fefdbdeb4ec8ad3e",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "cb7237763cad06b64c75",
+    "url": "/clicky-game/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6da6949eb84ecd671844",
+    "url": "/clicky-game/static/js/2.827207b6.chunk.js"
+  },
+  {
+    "revision": "cb7237763cad06b64c75",
+    "url": "/clicky-game/static/js/main.eec81c13.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
